2295bb7aa474f4ed6375a176843b1ae2a8549064
Emily Hu <hia8993@alfred.ece.northwestern.edu>
2026-02-15 18:20:32 -0600

    tried avoiding bank conflicts

--- opt_2dhisto.cu
+++ opt_2dhisto.cu
@@ -8,73 +8,42 @@
 
 __global__ void HistogramKernel(uint32_t *result_array, uint32_t *input_array, size_t inputWidth)
 {
-    const int numThreadsTotal = gridDim.x * gridDim.y * blockDim.x * blockDim.y; // threads total
     const int numThreadsBlock = blockDim.x * blockDim.y;
-    const int uniqueBlockIndex = blockIdx.y * gridDim.x + blockIdx.x;
     const int threadId = threadIdx.y * blockDim.x + threadIdx.x; // flattened within a block
-    const int globalTid = uniqueBlockIndex * numThreadsBlock + threadId; // flattened in whole invocation
-
     __shared__ uint32_t blockSharedHist[HISTO_HEIGHT * HISTO_WIDTH];
+    __shared__ uint32_t blockInputArray[32 * 32];
 
-    // if(globalTid == 1){
-    //     printf("numThreadsTotal %d\n", numThreadsTotal);
-    //     printf("GPU input array:\n");
-    //     for(int i = 0; i < INPUT_HEIGHT; i++){
-    //         for(int j = 0; j < INPUT_WIDTH; j++){
-    //             printf("%d ", input_array[i*inputWidth+j]);
-    //         }
-    //         printf("\n");
-    //     }
-    //     printf("\n\n");
-    // }
-    // init shared histogram for this block
-    // TODO: unroll?
     for(int pos = threadId; pos < HISTO_HEIGHT * HISTO_WIDTH; pos += numThreadsBlock){
         blockSharedHist[pos] = 0;
     }
 
+    // pull a tile of input array into shared memory
+    blockInputArray[threadId] = input_array[(blockIdx.y * 32 + threadIdx.y) * inputWidth + blockIdx.x * 32 + threadIdx.x];
     __syncthreads();
 
     // read from input and update block histogram
-    for(int pos = globalTid; pos < INPUT_HEIGHT * inputWidth; pos += numThreadsTotal){ // should have enough threads
-        int col = pos % inputWidth;
-        if(col < INPUT_WIDTH) {// overflow threads on edges
-            uint32_t data = input_array[pos];
-
-            if(blockSharedHist[data] < 255){ // try to limit unecessary atomicAdds
-                uint32_t test = atomicAdd(blockSharedHist + data, 1);
-                if(test >= 255){ // must test to not over add
-                    blockSharedHist[data] = 255;
+    for(int pos = 0; pos < 32 * 32; pos++){
+        int realcol = blockIdx.x * 32 + (pos % 32);
+        if(realcol < INPUT_WIDTH) {// check for overflow threads on edges
+            uint32_t data = blockInputArray[pos];
+            if(data % (32*32) == threadId % (32*32)){
+                if(blockSharedHist[data] < 255){ // try to limit unecessary atomicAdds
+                    uint32_t test = atomicAdd(blockSharedHist + data, 1);
+                    if(test >= 255){ // must test to not over add
+                        blockSharedHist[data] = 255;
+                    }
                 }
             }
         }
     }
     __syncthreads();
 
-    // if(globalTid == 1){
-    //     printf("print block histogram: \n");
-    //     for(int i = 0; i < 1024; i++)
-    //         printf("%d ", blockSharedHist[i]);
-    //     printf("\n\n");
-
-    // }
-
     for(int pos = threadId; pos < HISTO_HEIGHT * HISTO_WIDTH; pos += numThreadsBlock){
-        int res = atomicAdd(result_array + pos, blockSharedHist[pos]);
-        // if(globalTid == 1){
-        //     printf("added to global: %d\n", res);
-        // }
-        atomicMin(result_array + pos, 255); // clamp to 255
+        if(pos < HISTO_HEIGHT * HISTO_WIDTH){
+            int res = atomicAdd(result_array + pos, blockSharedHist[pos]);
+            atomicMin(result_array + pos, 255); // clamp to 255
+        }
     }
-    // if(globalTid == 1){
-
-    //     printf("print results array input: \n");
-    //     for(int i = 0; i < HISTO_HEIGHT * HISTO_WIDTH; i++){
-    //         printf("%d ", result_array[i]);
-    //     }
-    //     printf("\n\n");
-
-    // }
 }
 
 void opt_2dhisto(uint32_t *result_array, uint32_t *input_array, size_t inputWidth)
@@ -84,10 +53,10 @@ void opt_2dhisto(uint32_t *result_array, uint32_t *input_array, size_t inputWidt
        transfers must be done outside this function */
     // kernel invocation
     cudaMemset(result_array, 0, HISTO_HEIGHT*HISTO_WIDTH*sizeof(uint32_t));
-	dim3 dimBlock(16, 16); // 256
-	dim3 dimGrid((inputWidth + 16 - 1) / 16, (INPUT_HEIGHT + 16 - 1) / 16); // floor substitute ??
+	dim3 dimBlock(32, 32); // 1024 = 32 * 32, so 32 threads * 32 banks works out perfectly for columns. do how ever many rows for convenience
+	dim3 dimGrid((inputWidth + 32 - 1) / 32, (INPUT_HEIGHT + 32 - 1) / 32); // floor substitute ??
+	// dim3 dimGrid(1, 1);
 	HistogramKernel<<<dimGrid, dimBlock>>>(result_array, input_array, inputWidth);
-
 }
 
 /* Include below the implementation of any other functions you need */
